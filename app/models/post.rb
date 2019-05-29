0ec101c9dc32eb57ab7ea32c6a51dd358b90fe32
egd1753 <egd1753@gmail.com>
2019-05-28 22:01:43 -0500

    completed assignment 7 crud work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   has_many :comments
+
+  after_create :censure_post
+
+  def censure_post
+    if ( (self.id == 1) || ((self.id - 1) % 5 == 0) )
+      self.title = "SPAM"
+      self.save
+    end
+  end
+
 end
