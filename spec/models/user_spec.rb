ef9dd6da955d54157d6cff90b2c8b42a88ea549c
egd1753 <egd1753@gmail.com>
2019-06-05 22:13:28 -0500

    completed checkpoint 12 auth user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
 
   # Shoulda tests for email
   it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
 
@@ -25,8 +25,8 @@ RSpec.describe User, type: :model do
 
 
   describe "invalid user" do
-    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+    let(:user_with_invalid_name) { User.new(name: "", email: "random@bloccit.com") }
+    let(:user_with_invalid_email) { User.new(name: "Random User", email: "") }
 
     it "should be an invalid user due to blank name" do
       expect(user_with_invalid_name).to_not be_valid
