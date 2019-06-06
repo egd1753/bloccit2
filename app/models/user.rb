5fdff1f3e8ff70b9e9d61667bc793e2f9310e0f7
egd1753 <egd1753@gmail.com>
2019-06-05 23:54:03 -0500

    worked on assignment 12 auth user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,27 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_names
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+
   validates :password, presence: true, length: { minimum: 6 }, if: -> { password_digest.nil? }
   validates :password, length: { minimum: 6 }, allow_blank: true
+
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: true },
             length: { minimum: 3, maximum: 254 }
+
   has_secure_password
+
+  def format_names
+    if name
+      full_name = []
+      name.split.each do |element|
+        full_name.push(element.capitalize)
+      end
+      self.name = full_name.join(" ")
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,11 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+
+    it "should format the user's name" do
+      user.name = "bud studdard"
+      expect(user.name).to eq "Bud Studdard"
+    end
   end
 
 
