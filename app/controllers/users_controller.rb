3cd4a808796a4c226323f9dd7ffe7ca644b21ece
egd1753 <egd1753@gmail.com>
2019-06-08 17:07:35 -0500

    completed assignment 13 auth signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,20 @@ class UsersController < ApplicationController
       end
   end
 
+
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  helper_method :user_params
+
 end
