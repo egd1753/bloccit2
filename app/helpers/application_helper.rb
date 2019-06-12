e4f87a6370af65cf4095a7a68b3a4b8badf2d747
egd1753 <egd1753@gmail.com>
2019-06-12 09:43:38 -0500

    finished checkpoint 16 authorization work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
+
   def form_group_tag(errors, &block)
     css_class = 'form-group'
     css_class << ' has-error' if errors.any?
     content_tag :div, capture(&block), class: css_class
   end
+
 
   def avatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
