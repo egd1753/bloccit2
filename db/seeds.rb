1e2dfb1c689b45ef097e07ef5fc204204ebad7cf
egd1753 <egd1753@gmail.com>
2019-06-18 18:02:21 -0500

    started checkpoint work on voting

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ end
 topics = Topic.all
 
 50.times do
-  Post.create!(
+  post = Post.create!(
     user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
@@ -33,6 +33,8 @@ topics = Topic.all
   )
 end
 posts = Post.all
+post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 
 100.times do
   Comment.create!(
@@ -62,3 +64,4 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
